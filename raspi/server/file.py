33b2342f1d44d057505ea57c4a60ee672d76a606
RAFAIL ANDREOU <rafailandreou123@gmail.com>
2025-12-13 10:24:42 +0200

    fix json format

--- raspi/server/file.py
+++ raspi/server/file.py
@@ -1,5 +1,9 @@
+import json
+
 def save_configure_json(configuration: dict):
-    with open("raspi/server/configure.json", "w") as f:
-        content = f.write(str(configuration))
+    with open("raspi/server/configure.json", "w+") as f:
+        content = f.write(json.dumps(configuration))
         print("✅ Configuration saved to raspi/server/configure.json")
+       
+        
         
